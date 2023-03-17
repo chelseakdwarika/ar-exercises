323b1d732fcc2813d1feb4cbd4a1ca7695abcabd
Chelsea Dwarika <chelseakristina@live.com>
2023-03-17 17:33:12 +0000

    write validation for Employee and Store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+#VALIDATION
+
+#Employees must have a FIRST and LAST name, hourly_rate(integer between 40-200), belong to a store
+#STORES have a name with min 3 chars, annual_revenue(integer >= 0)
+
+#Ask user for store name and put into a variable
+
+#Create error testing store with that name ^ but leave out other criteria and store error messages
