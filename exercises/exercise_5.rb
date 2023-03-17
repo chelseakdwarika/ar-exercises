1b00a18c222c8b91725f5c6909816a1a9006281d
Chelsea Dwarika <chelseakristina@live.com>
2023-03-17 17:05:35 +0000

    sum and average total revenues, return million dolla stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,13 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+#output total revenue for ALL stores using .sum
+@total_revenue = Store.sum(:annual_revenue)
+puts "ANNUAL REVENUE FROM ALL STORES: #{@total_revenue}"
+
+#output average annual revenue for ALL stores
+@total_revenue = Store.average(:annual_revenue)
+puts "AVERAGE Annual Revenue: #{@total_revenue}"
+
+#output count of stores with +$1M in annual sales
+puts Store.where("annual_revenue > '1000000'").count()
